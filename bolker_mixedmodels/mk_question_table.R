349ceafd0d2431969b58860f071c7d1572da1dbc
Ben Bolker <bbolker@gmail.com>
2021-11-05 21:42:02 -0400

    answers to questions

---
+++ bolker_mixedmodels/mk_question_table.R
@@ -0,0 +1,18 @@
+xx <- read.csv("Bolker_PollEV_Questions-1.csv")
+names(xx) <- c("Q", "A", "junk")
+outfile <- "QA.md"
+
+
+hdr <- "---
+title: 'Questions and answers'
+---
+"
+
+cat(hdr, "\n", file = outfile, append = FALSE)
+for (i in 1:nrow(xx)) {
+  cat(file = outfile, append = TRUE,
+      i, ". ",
+      xx$Q[i], "\n\n",
+      "**Answer**: \n",
+      xx$A[i], "\n\n")
+}
